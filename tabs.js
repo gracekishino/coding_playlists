1f68701fd1b1b0d7a37f8c7ecf8b2cd96906e79f
Jainil Jain <112818722+Jainil2004@users.noreply.github.com>
2024-02-20 19:31:47 +0530

    Apply suggestions from code review
    Co-authored-by: Grace Kishino <35483611+gracekishino@users.noreply.github.com>

--- tabs.js
+++ tabs.js
@@ -1,21 +1,8 @@
-function openPlayer(cityName) {
+function openPlayer(playerName) {
     var i;
     var x = document.getElementsByClassName("player_tab");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
-    document.getElementById(cityName).style.display = "block";
-}
-
-window.onload = function() {
-    var navRefer = document.getElementById('nav_refer');
-    var header = document.querySelector('header');
-
-    navRefer.addEventListener('mouseover', function() {
-        header.classList.add('shadow');
-    });
-
-    navRefer.addEventListener('mouseout', function() {
-        header.classList.remove('shadow');
-    });
+    document.getElementById(playerName).style.display = "block";
 }
