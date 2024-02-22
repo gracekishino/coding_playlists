ea367290747d86d6cd0312fda4ae68d811306f5b
Anmol-Gup <sp430661@gmail.com>
2024-02-22 15:47:36 +0530

    Add highlighted button

--- tabs.js
+++ tabs.js
@@ -1,8 +1,22 @@
 function openPlayer(playerName) {
-    var i;
-    var x = document.getElementsByClassName("player_tab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+  var i;
+  var x = document.getElementsByClassName("player_tab");
+  for (i = 0; i < x.length; i++) {
+    console.log(x[i])
+    x[i].style.display = "none";
+  }
+  document.getElementById(playerName).style.display = "block";
+
+  const buttons = document.getElementsByTagName('button')
+  
+  Array.from(buttons).forEach(element => {
+    if (element.id === `${playerName}Btn`) {
+      element.style.backgroundColor = '#e54b4b'
+      element.style.color = 'black'
     }
-    document.getElementById(playerName).style.display = "block";
+    else {
+      element.style.backgroundColor = 'rgba(40, 40, 40, 0.7)'
+      element.style.color = '#ff5e5e'
+    }
+  });
 }
